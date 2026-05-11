6ab2a2dbd9877e515b0ae30a80938afcecc93893
LeCaseus <263502040+LeCaseus@users.noreply.github.com>
2026-05-12 10:06:35 +1200

    Untrack all files

--- .config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fastfetch
-    set fish_greeting
-    starship init fish | source
-    alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-end
